c859fb23576c4b2975de831400a5396c32b3768b
ddanijel <danijel.fon@gmail.com>
2020-07-21 23:11:01 +0200

    switching back to istio

--- 10-alb-listener-rules.tf
+++ 10-alb-listener-rules.tf
@@ -13,27 +13,27 @@ resource "aws_alb_listener_rule" "chartmuseum_alb_listener_rule" {
   }
 }
 
-resource "aws_alb_listener_rule" "mp_api_gateway_alb_listener_rule" {
-  listener_arn = aws_lb_listener.application_lb_https_listener.arn
-  action {
-    type             = "forward"
-    target_group_arn = aws_alb_target_group.mp_api_gateway_target_group.arn
-  }
-  condition {
-    host_header {
-      values = [
-        local.dns_params.base_domain_name
-      ]
-    }
-  }
-
-  condition {
-    path_pattern {
-      values = [
-        "/graphql",
-        "/auth/challenge",
-      ]
-    }
-  }
-
-}
+//resource "aws_alb_listener_rule" "mp_api_gateway_alb_listener_rule" {
+//  listener_arn = aws_lb_listener.application_lb_https_listener.arn
+//  action {
+//    type             = "forward"
+//    target_group_arn = aws_alb_target_group.mp_api_gateway_target_group.arn
+//  }
+//  condition {
+//    host_header {
+//      values = [
+//        local.dns_params.base_domain_name
+//      ]
+//    }
+//  }
+//
+//  condition {
+//    path_pattern {
+//      values = [
+//        "/graphql",
+//        "/auth/challenge",
+//      ]
+//    }
+//  }
+//
+//}
